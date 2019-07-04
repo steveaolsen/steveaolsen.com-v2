801e572f0ca437811de875942edfcb14f1ea1c3f
steveaolsen <steveaolsen@gmail.com>
2019-07-03 23:28:25 -0400

    x

--- pages/send_form_email.php
+++ pages/send_form_email.php
@@ -4,46 +4,37 @@
 <?php
 if(isset($_POST['email'])) {
  
-    //IMPORTANT!!!don't forget the form only works on a server with php, 
-    //it wont run on the conputer
-    $email_to = "steveaolsen@gmail.com";
-    $email_subject = "Contact from steveaolsen.com";
+  $email_to = "steveaolsen@gmail.com";
+  $email_subject = "Contact from steveaolsen.com";
+
+  function died($error) {
+    // your error code can go here
+    echo "Sorry, but there were error(s) found with the form you submitted. ";
+    echo "These errors appear below.<br /><br />";
+    echo $error."<br /><br />";
+    echo "Please go back and fix these errors.<br /><br />";
+    die();
+  }
+
+  // validation expected data exists
+  if(!isset($_POST['first_name']) || !isset($_POST['last_name']) || !isset($_POST['email']) || !isset($_POST['telephone']) || !isset($_POST['comments'])) {
+    died('Sorry, but there appears to be a problem with the form you submitted.');       
+  }
  
-    function died($error) {
-        // your error code can go here
-        echo "Sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
- 
-    // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
-        died('Sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
-     
- 
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  $first_name = $_POST['first_name']; // required
+  $last_name = $_POST['last_name']; // required
+  $email_from = $_POST['email']; // required
+  $telephone = $_POST['telephone']; // not required
+  $comments = $_POST['comments']; // required
+
+  $error_message = "";
+  $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
   if(!preg_match($email_exp,$email_from)) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
+  /*
+  $string_exp = "/^[A-Za-z .'-]+$/";
  
   if(!preg_match($string_exp,$first_name)) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
@@ -56,27 +47,25 @@ if(isset($_POST['email'])) {
   if(strlen($comments) < 2) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   }
- 
+ */
   if(strlen($error_message) > 0) {
     died($error_message);
   }
  
-    $email_message = "Form details below.\n\n";
+  $email_message = "Form details below.\n\n";
  
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-     
- 
+  function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Telephone: ".clean_string($telephone)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
- 
+  }
+}
+
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
@@ -87,8 +76,3 @@ $headers = 'From: '.$email_from."\r\n".
 Thank you for contacting me. I will be in touch with you very soon.
 
 <a href="../home.html">Back to the Homepage</a>
- 
-//<?php
- 
-}
-?>
